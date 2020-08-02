8b225d7b29ea088e732e591bf262b56b9e08e111
Fly <393830887@qq.com>
2020-08-02 22:06:59 +0800

    打包生成

---
+++ Assets/Editor/EditorTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class EditorTest
+{
+    private static string DefaultBuildPath
+    {
+        get
+        {
+            return Path.GetDirectoryName(Application.dataPath) + "/AutoTool/Build/AutoBuild";
+        }
+    }
+    
+    private static Dictionary<string, string> DueArguments()
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        var paramDict = new Dictionary<string, string>();
+
+        string command = null;
+        foreach (var arg in args)
+        {
+            if (arg.StartsWith("-"))
+            {
+                command = arg;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(command))
+                {
+                    continue;                    
+                }
+                
+                paramDict[command] = paramDict.ContainsKey(command) ? paramDict[command] + " " + arg : arg;
+            }
+        }
+
+        return paramDict;
+    }
+
+    private static string GetBuildPath(Dictionary<string, string> paramDict)
+    {
+        string path;
+        if (!paramDict.TryGetValue("-buildPath", out path))
+        {
+            path = DefaultBuildPath;
+        }
+
+        return path;
+    }
+    
+    [MenuItem("Test/Build")]
+    private static void Build()
+    {
+        var paramDict = DueArguments();
+        var buildPath = GetBuildPath(paramDict);
+        Debug.Log(buildPath);
+
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath, EditorUserBuildSettings.activeBuildTarget,
+            BuildOptions.Development | BuildOptions.ShowBuiltPlayer | BuildOptions.CompressWithLz4);
+    }
+}
---
+++ AutoTool/Build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+curPath=$(readlink -f "$(dirname "$0")")
+echo $curPath
+
+Unity_Path="C:/Program Files/Unity/Hub/Editor/2018.4.23f1/Editor/Unity.exe"
+Project_Path="E:/Workspace/Auto"
+Log_Path="${curPath}/BuildLog.log"
+Execete_Method="EditorTest.Build"
+Build_Path="${curPath}/Build/AutoBuildBySh"
+
+
+"$Unity_Path" -projectPath $Project_Path -logFile $Log_Path -nographics -quit -batchmode -executeMethod $Execete_Method -buildPath $Build_Path
+
+cat $Log_Path
